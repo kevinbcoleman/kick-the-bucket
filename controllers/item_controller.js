d114cc90a9895d2baa597421ccf93eed22034bfe
Rachel Taima <racheltaima@Rachels-MacBook-Air.local>
2021-05-12 15:35:28 -0700

    created the items controller. Have not tested it on Postman

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -3,9 +3,48 @@ const bucketItems = express.Router()
 const Item = require('../models/items.js')
 const User = require('../models/users.js')
 
-//Display Route:
+
+//Index Route:
 bucketItems.get('/', (req,res) => {
-  res.send('This is the display for bucket list items.')
+  Item.find({}, (error, foundItems) => {
+    res.json(foundItems)
+  })
+})
+
+//create route:
+bucketItems.post('/', (req,res) => {
+  Item.create(req.body, (error, createdItem) => {
+    Item.find({}, (error, foundItems) => {
+      res.json(foundItems)
+    })
+  })
+})
+
+//edit and update route:
+bucketItems.put('/:id', (req,res) => {
+  Item.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new: true},
+    (error, updatedItem) => {
+      if(error){
+        res.send(error)
+      }else{
+        Item.find({}, (error, foundItems) => {
+          res.json(foundItems)
+        })
+      }
+    }
+  )
+})
+
+//delete route:
+bucketItems.delete('/:id', (req,res) => {
+  Item.findByIdAndRemove(req.params.id, (error, deletedItem) => {
+    Item.find({}, (error, foundItems) => {
+      res.json(foundItems)
+    })
+  })
 })
 
 module.exports = bucketItems
