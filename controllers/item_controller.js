9ac8c86e351360a7eff510d65de5e07c5b07d421
Rachel Taima <racheltaima@Rachels-MacBook-Air.local>
2021-05-16 18:37:15 -0700

    bye user

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const bucketItems = express.Router()
 const Item = require('../models/items.js')
-const User = require('../models/users.js')
-const itemSeed = require('../models/item_seed.js')
-
+// const User = require('../models/users.js')
 
 
 //Index Route:
-bucketItems.get('/', (req, res) => {
+bucketItems.get('/', (req,res) => {
   Item.find({}, (error, foundItems) => {
     res.json(foundItems)
   })
@@ -21,73 +19,35 @@ bucketItems.get('/', (req, res) => {
 // })
 
 //create route:
-bucketItems.post('/', (req, res) => {
-  let currentUser = req.body.currentUser
-
-  if (currentUser) {
-    User.findOne({ username: currentUser.username }, (error, foundUser) => {
-      console.log(foundUser)
-      Item.create(req.body, (error, createdItem) => {
-        foundUser.bucketItems.push(createdItem)
-        foundUser.save((error, data) => {
-          res.json(data)
-        })
-      })
-    })
-  } else {
+bucketItems.post('/', (req,res) => {
     Item.create(req.body, (error, createdItem) => {
       Item.find({}, (error, foundItems) => {
         res.json(foundItems)
       })
-    })
-  }
+  })
 })
 
 //edit and update route:
-bucketItems.put('/:id', (req, res) => {
-  let currentUser = req.body.currentUser
-
-  if (currentUser) {
+bucketItems.put('/:id', (req,res) => {
     Item.findByIdAndUpdate(
       req.params.id,
       req.body,
-      { new: true },
+      {new: true},
       (error, updatedItem) => {
-        User.findOne({ username: currentUser.username }, (error, foundUser) => {
-          foundUser.bucketItems.id(req.params.id).remove()
-          foundUser.bucketitems.push(updatedItem)
-          foundUser.save((error, data) => {
-            res.json(data)
-          })
-        })
-      }
-    )
-  } else {
-    Item.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true },
-      (error, updatedItem) => {
-        Item.find({}, (error, foundItems) => {
+        Item.find({}, (error,foundItems) => {
           res.json(foundItems)
         })
       }
-    )
-  }
+  )
 })
 
 //delete route:
-bucketItems.delete('/:id', (req, res) => {
+bucketItems.delete('/:id', (req,res) => {
   Item.findByIdAndRemove(req.params.id, (error, deletedItem) => {
     Item.find({}, (error, foundItems) => {
       res.json(foundItems)
     })
   })
 })
-
-//Seed Insertion: Commented out to prevent reseeding.
-
-// Item.insertMany(itemSeed)
-
 
 module.exports = bucketItems
